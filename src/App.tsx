47597b41fb47ff99e4d1f273aa437c403d0ac14b
JIN <0626na@gmail.com>
2022-03-02 12:26:39 +0900

    환율 표시기 개발중.. 아직 초기단계

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+
+interface Change {
+  result: number;
+  cur_unit: string;
+  ttb: string;
+  tts: string;
+  deal_bas_r: string;
+  bkpr: string;
+  yy_efee_r: string;
+  ten_dd_efee_r: string;
+  kftc_bkpr: string;
+  kftc_deal_bas_r: string;
+  cur_nm: string;
+}
+
+const fetchExchange = async () => {
+  const auth = "FgYNZJ7yTBdsSsUAMTxqGaBP0wNkwGsR";
+  const data = await (
+    await fetch(
+      `/site/program/financial/exchangeJSON?authkey=${auth}&data=AP01`
+    )
+  ).json();
+
+  return data;
+};
 
 function App() {
+  const { isLoading, data } = useQuery<Change[]>("exchange", fetchExchange);
+  const [value, setValue] = useState("");
+  const onChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setValue(event.currentTarget.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading ? (
+        <h1>Loading</h1>
+      ) : (
+        <>
+          <div>
+            <label htmlFor="exchange">통화명 </label>
+            <select name="countries1" id="exchange1" onChange={onChange}>
+              {data?.map((data) => (
+                <option key={data.cur_nm} value={data.bkpr}>
+                  {data.cur_nm} {data.cur_unit}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <h2>첫번째국가: {value}</h2>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
---
+++ src/ExchangeCal.tsx
@@ -0,0 +1,5 @@
+const ExchaneCalculator = () => {
+  return <></>;
+};
+
+export default ExchaneCalculator;
