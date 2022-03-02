ef53e01808fd2ead5d95752aee2b14079007165c
JIN <0626na@gmail.com>
2022-03-02 20:45:37 +0900

    프록시 문제 해결중

--- src/api.tsx
+++ src/api.tsx
@@ -1,7 +1,9 @@
 export const fetchExchange = async () => {
   const auth = "FgYNZJ7yTBdsSsUAMTxqGaBP0wNkwGsR";
   const data = await (
-    await fetch(`site/program/financial/exchangeJSON?authkey=${auth}&data=AP01`)
+    await fetch(
+      `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${auth}&data=AP01`
+    )
   ).json();
   console.log(data);
   return data;
